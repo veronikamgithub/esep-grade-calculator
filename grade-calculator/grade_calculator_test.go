39ffd727f4fd2d3ec0bbbf4cec47ee9898ef923f
Veronika Matos <Veronika.matos01@icloud.com>
2025-10-02 14:12:51 -0400

    added a couple of tests to push the bounds and expose possible errors, but the tests passed

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -1,6 +1,8 @@
 package esepunittests
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetGradeA(t *testing.T) {
 	expected_value := "A"
@@ -49,3 +51,35 @@ func TestGetGradeF(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+func TestGetGradeNegative(t *testing.T) {
+	expected_value := "F" // need to put the letter grade
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", -2, Assignment)
+	gradeCalculator.AddGrade("exam 1", -5, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", -8, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value { // compares to a string
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+} // edge case for negative values since main code doesn't exclude
+
+func TestGetGradeAPlus(t *testing.T) {
+	expected_value := "A"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 120, Assignment)
+	gradeCalculator.AddGrade("exam 1", 105, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 115, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+} // edge case for above 100 (A)
